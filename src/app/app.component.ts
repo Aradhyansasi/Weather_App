bf7c485f0ff09364d01804e6439be86057b98fa3
Aradhyansasi <aradhya.n.sasi101@gmail.com>
2023-05-28 17:08:24 +0530

    Weather app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+
+
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { SelectItem } from 'primeng/api';
+import { map } from 'rxjs/operators';
+import { WeatherService } from 'src/service/weather.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather-app';
+
+
+
+  weatherConditions: any[]=[];
+  selectedCondition: any;
+  forecastData: any[] = [];
+  filteredData: any[] =[];
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherConditions().subscribe({
+      next: (data) => {
+        console.log("data===>", data)
+        console.log("selectedCondition ==>",this.selectedCondition);
+        
+        if (data && data.wethercondition && Array.isArray(data.wethercondition)) {
+          this.weatherConditions = data.wethercondition.map((condition: { condition: any; }) => condition.condition);
+        } else {
+          console.log('Invalid weather data:', data);
+        }
+        console.log("weatherConditions ==>",this.weatherConditions);
+      },
+      error: (error) => {
+        console.log('Error fetching weather conditions:', error);
+      }
+      
+    });
+
+    this.weatherService.getWeatherData().subscribe((forecast) => {
+      this.forecastData = forecast.data;
+      this.filteredData = this.forecastData;
+    });
+  }
+
+  
+
+  filterData() {
+    if (this.selectedCondition && this.selectedCondition !== 'All') {
+      this.filteredData = this.forecastData.filter(item => item.condition === this.selectedCondition);
+    } else {
+      this.filteredData = this.forecastData;
+    }
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DropdownModule } from 'primeng/dropdown';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { HttpClientModule } from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+
+
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DropdownModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
